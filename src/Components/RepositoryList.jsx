dff8f33eff2bc3cf8b6d1d66af40e4ab9e97a778
elton de campos <eltoncampos36@gmail.com>
2021-03-09 01:06:48 -0300

    chore: add types react-dom

--- src/Components/RepositoryList.jsx
+++ src/Components/RepositoryList.jsx
@@ -1,29 +1,26 @@
 import { RepositoryItem } from "./RepositoryItem";
-import '../styles/repositories.scss';
+import "../styles/repositories.scss";
 import { useEffect, useState } from "react";
-
 
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
 
-
   useEffect(() => {
-    fetch('https://api.github.com/orgs/rocketseat/repos')
-      .then(response => response.json())
-      .then(data => setRepositories(data));
-  },[])
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
 
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-
         {repositories.map((repository) => {
-          
-          return <RepositoryItem key={repository.name} repository={repository} />
+          return (
+            <RepositoryItem key={repository.name} repository={repository} />
+          );
         })}
-        
       </ul>
     </section>
   );
