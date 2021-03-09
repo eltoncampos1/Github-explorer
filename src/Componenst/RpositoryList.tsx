c24b9c4c5c213bde8ade2e3eaaf6fca6aaadae83
elton de campos <eltoncampos36@gmail.com>
2021-03-08 22:03:57 -0300

    feat: create repository list componet

---
+++ src/Componenst/RpositoryList.tsx
@@ -0,0 +1,30 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms ini react</p>
+          <a href="#">Acessar repostórios</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms ini react</p>
+          <a href="#">Acessar repostórios</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms ini react</p>
+          <a href="#">Acessar repostórios</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms ini react</p>
+          <a href="#">Acessar repostórios</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
